a0b3777a5ae509c6033f17fc06b90583002c6b5d
asbp <guungofficial@gmail.com>
2022-10-23 12:52:31 +0700

    set version

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,8 @@ import u_num from "./num";
 import u_tof from "./tof";
 
 const tilde = (function () {
-  var pjson = require("./package.json");
-
-  let version = pjson.version;
-
   const funcExports = {
-    version,
+    version: "0alpha",
     tof: u_tof,
     number: u_num,
     color: u_color,
